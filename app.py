04fc977fea340ee669d31a36d91c92cdc6ff1400
Angel Fco <angel02fco@gmail.com>
2024-07-25 09:48:22 -0600

    actualizacion

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, jsonify, send_file
+from flask import Flask, request, render_template, jsonify
 import joblib
 import pandas as pd
 import logging
@@ -8,10 +8,11 @@ app = Flask(__name__)
 # Configurar el registro
 logging.basicConfig(level=logging.DEBUG)
 
-# Cargar el modelo entrenado
-model = joblib.load('concrete_modelo.pkl')
-scaler = joblib.load('concrete_escalado.pkl')
-app.logger.debug('Modelo cargado correctamente.')
+# Cargar el modelo entrenado y el escalador
+model = joblib.load('modelo_gradient_boosting_jireh.pkl')
+scaler = joblib.load('dataFrameScalado_jireh.pkl')
+
+app.logger.debug('Modelo y escalador cargados correctamente.')
 
 @app.route('/')
 def home():
@@ -20,51 +21,46 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        # Obtener los datos enviados en el request
-        cement = float(request.form['cement'])
-        blast = float(request.form['blast'])
-        water =float(request.form['water'])
-        superplasticizer =float(request.form['superplasticizer'])
-        age =float(request.form['age'])
+        # Obtener los datos enviados en el request en formato JSON
+        data = request.get_json()
 
+        tiempo_de_espera = data['tiempo_espera']
+        duracion_de_atencion = data['duracion_atencion']
+        costo_servicio = data['costo_servicio']
+        calidad_atencion = data['calidad_atencion']
+        instalaciones = data['instalaciones']
+        comunicacion = data['comunicacion']
+        facilidad_de_pago = data['facilidad_de_pago']
+
+        # Crear un DataFrame con todas las características necesarias
         input_data = pd.DataFrame({
-            'Cement': [cement],
-            'Blast Furnace Slag': [blast],
-            'Fly Ash': [0],
-            'Water': [water],
-            'Superplasticizer': [superplasticizer],
-            'Coarse Aggregate': [0],
-            'Fine Aggregate': [0],
-            'Age (day)': [age]
+            'Tipo_Servicio': [0],
+            'Tiempo_Espera': [tiempo_de_espera],
+            'Duración_Atención': [duracion_de_atencion],
+            'Costo_Servicio': [costo_servicio],
+            'Calidad_Atención': [calidad_atencion],
+            'Instalaciones': [instalaciones],
+            'Comunicación': [comunicacion],
+            'Facilidad_Pago': [facilidad_de_pago],
+            'Experiencia_Cliente': [0],
+            'Recomendaciones': [0],
         })
-
         # Escalar los datos de entrada
         scaled_data = scaler.transform(input_data)
 
         # Seleccionar solo las características usadas para el modelo
-        scaled_data_for_prediction = scaled_data[:, [0,1,2,3,7]]  # Asegúrate de que estos índices son correctos
+        scaled_data_for_prediction = scaled_data[:, [1,2,3,4,5,6,7]]  # Asegúrate de que estos índices son correctos
+        print(scaled_data_for_prediction)
 
         # Realizar la predicción con los datos escalados
         prediccion = model.predict(scaled_data_for_prediction)
 
         # Devolver la predicción como JSON
-        prediction_value = round(float(prediccion[0]), 2)
-
+        prediction_value = (prediccion[0])  # Convertir a int para que sea JSON serializable
         return jsonify({'prediction': prediction_value})
-
     except Exception as e:
         app.logger.error(f'Error en la predicción: {str(e)}')
         return jsonify({'error': str(e)}), 400
-
-@app.route('/download')
-def download_file():
-    try:
-        # Ruta del archivo CSV
-        filepath = "Concrete Compressive Strength.csv"
-        return send_file(filepath, as_attachment=True)
-    except Exception as e:
-        app.logger.error(f'Error al descargar el archivo: {str(e)}')
-        return jsonify({'error': 'No se pudo descargar el archivo.'}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
